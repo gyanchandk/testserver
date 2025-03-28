4d2472e81e98ce2072fc3bdbc329b14c7d7654b5
gyanchandk <gyanchand1294@gmail.com>
2025-03-28 16:00:20 +0530

    added new endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
 });
 
+app.get("/api", (req, res) => {
+  res.send("api home page");
+});
+
 const port = process.env.PORT || 8000;
 
 app.listen(port,()=>console.log(`listening of post ${port}`));
